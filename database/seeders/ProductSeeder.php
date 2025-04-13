e2652a95f35c4da78d3bb2f163d6ed0be325576b
Wage Folabessy <mbappepsg336@gmail.com>
2025-04-13 18:01:01 +0700

    update product seeder and add product images

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -28,19 +28,19 @@ class ProductSeeder extends Seeder
                 'description'    => 'Kemeja pria model slim fit.',
                 'images'         => [
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\Indigo-slub-workshirt-LS-1.jpg',
+                        'image'       => public_path('product-as-denim/Indigo-slub-workshirt-LS-1.jpg'),
                         'is_primary' => true,
                     ],
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\Indigo-slub-workshirt-LS-2.jpg',
+                        'image'       => public_path('product-as-denim/Indigo-slub-workshirt-LS-2.jpg'),
                         'is_primary' => false,
                     ],
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\Indigo-slub-workshirt-LS-3.jpg',
+                        'image'       => public_path('product-as-denim/Indigo-slub-workshirt-LS-3.jpg'),
                         'is_primary' => false,
                     ],
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\Indigo-slub-workshirt-LS-8.jpg',
+                        'image'       => public_path('product-as-denim/Indigo-slub-workshirt-LS-8.jpg'),
                         'is_primary' => false,
                     ],
                 ],
@@ -55,15 +55,15 @@ class ProductSeeder extends Seeder
                 'description'    => 'Kemeja pria model slim fit.',
                 'images'         => [
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\Herringbone-Western-Shirt-1.jpg',
+                        'image'       => public_path('product-as-denim/Herringbone-Western-Shirt-1.jpg'),
                         'is_primary' => true,
                     ],
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\Herringbone-Western-Shirt-2.jpg',
+                        'image'       => public_path('product-as-denim/Herringbone-Western-Shirt-1.jpg'),
                         'is_primary' => false,
                     ],
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\Herringbone-Western-Shirt-3-510x638.jpg',
+                        'image'       => public_path('product-as-denim/Herringbone-Herringbone-Western-Shirt-3-510x638.jpg'),
                         'is_primary' => false,
                     ],
                 ],
@@ -79,15 +79,15 @@ class ProductSeeder extends Seeder
                 'description'    => 'Celana.',
                 'images'         => [
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\Katalog-GAMA-1-2000x2500.jpg',
+                        'image'       => public_path('product-as-denim/Katalog-GAMA-1-2000x2500.jpg'),
                         'is_primary' => true,
                     ],
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\Katalog-GAMA-3-2000x2500.jpg',
+                        'image'       => public_path('product-as-denim/Katalog-GAMA-3-2000x2500.jpg'),
                         'is_primary' => false,
                     ],
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\Katalog-GAMA-4-1229x1536.jpg',
+                        'image'       => public_path('product-as-denim/Katalog-Katalog-GAMA-4-1229x1536.jpg'),
                         'is_primary' => false,
                     ],
                 ],
@@ -102,15 +102,15 @@ class ProductSeeder extends Seeder
                 'description'    => 'Celana',
                 'images'         => [
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\Yasuke-COVER-1-1.jpg',
+                        'image'       => public_path('product-as-denim/Yasuke-COVER-1-1.jpg'),
                         'is_primary' => true,
                     ],
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\Yasuke-7.jpg',
+                        'image'       => public_path('product-as-denim/Yasuke-7.jpg'),
                         'is_primary' => false,
                     ],
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\Yasuke-10.jpg',
+                        'image'       => public_path('product-as-denim/Yasuke-10.jpg'),
                         'is_primary' => false,
                     ],
                 ],
@@ -125,15 +125,15 @@ class ProductSeeder extends Seeder
                 'description'    => 'Kaos',
                 'images'         => [
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\Tshirt-Duck-white-1.jpg',
+                        'image'       => public_path('product-as-denim/Tshirt-Duck-white-1.jpg'),
                         'is_primary' => true,
                     ],
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\Tshirt-Duck-white-2.jpg',
+                        'image'       => public_path('product-as-denim/Tshirt-Duck-white-2.jpg'),
                         'is_primary' => false,
                     ],
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\Tshirt-Duck-white-4.jpg',
+                        'image'       => public_path('product-as-denim/Tshirt-Duck-white-4.jpg'),
                         'is_primary' => false,
                     ],
                 ],
@@ -148,11 +148,11 @@ class ProductSeeder extends Seeder
                 'description'    => 'Kaos',
                 'images'         => [
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\greenday.jpeg',
+                        'image'       => public_path('product-as-denim/greenday.jpeg'),
                         'is_primary' => true,
                     ],
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\green-day-shirts.png',
+                        'image'       => public_path('product-as-denim/green-day-shirts.png'),
                         'is_primary' => false,
                     ],
                 ],
@@ -167,9 +167,122 @@ class ProductSeeder extends Seeder
                 'description'    => 'Sepatu',
                 'images'         => [
                     [
-                        'image'       => 'c:\Users\USER\Downloads\product-as-denim\sepatu.jpeg',
+                        'image'       => public_path('product-as-denim/sepatu.jpeg'),
                         'is_primary' => true,
                     ]
+                ],
+            ],
+            [
+                'product_name'   => 'LUXE PURPLE 17oz',
+                'category_id'    => 1,
+                'original_price' => 1150000,
+                'size'           => '29 SLIM STRAIGHT - Medium Rise',
+                'stock'          => 1,
+                'brand'          => "Wingman",
+                'color'          => "Purple, Indigo, Ungu",
+                'weight'         => 100,
+                'description'    => 'Pada umumnya denim menggunakan benang kapas berwarna putih/natural yang di warnai ke dalam pewarna indigo. Seiring pemakaian, denim akan luntur dan permukaan luar denim akan kembali berwarna putih. Kami menambah 1 PROSES PEWARNAAN, sebelum diwarnai indigo, benang kapas terlebih dahulu diwarnai UNGU, sehingga ketika denim digunakan fading permukaan luar akan menjadi warna UNGU.',
+                'images'         => [
+                    [
+                        'image'       => public_path('product-as-denim/Katalog-Luxe-Purple-Core-1-1.jpg'),
+                        'is_primary' => true,
+                    ],
+                    [
+                        'image'       => public_path('product-as-denim/Katalog-Luxe-Purple-Core-3.jpg'),
+                        'is_primary' => false,
+                    ],
+                    [
+                        'image'       => public_path('product-as-denim/Katalog-Luxe-Purple-Core-7.jpg'),
+                        'is_primary' => false,
+                    ],
+                    [
+                        'image'       => public_path('product-as-denim/Katalog-Luxe-Purple-Core-9.jpg'),
+                        'is_primary' => false,
+                    ],
+                ],
+            ],
+            [
+                'product_name'   => 'LUXE RED 17oz',
+                'category_id'    => 1,
+                'original_price' => 1150000,
+                'size'           => '29 SLIM STRAIGHT - Medium Rise',
+                'stock'          => 1,
+                'brand'          => "Wingman",
+                'color'          => "Red, Indigo, Merah",
+                'weight'         => 100,
+                'description'    => 'Pada umumnya denim menggunakan benang kapas berwarna putih/natural yang di warnai ke dalam pewarna indigo. Seiring pemakaian, denim akan luntur dan permukaan luar denim akan kembali berwarna putih. Kami menambah 1 PROSES PEWARNAAN, sebelum diwarnai indigo, benang kapas terlebih dahulu diwarnai UNGU, sehingga ketika denim digunakan fading permukaan luar akan menjadi warna UNGU.',
+                'images'         => [
+                    [
+                        'image'       => public_path('product-as-denim/Katalog-Luxe-Red-Core-1-1.jpg'),
+                        'is_primary' => true,
+                    ],
+                    [
+                        'image'       => public_path('product-as-denim/Katalog-Luxe-Red-Core-3.jpg'),
+                        'is_primary' => false,
+                    ],
+                    [
+                        'image'       => public_path('product-as-denim/Katalog-Luxe-Red-Core-7.jpg'),
+                        'is_primary' => false,
+                    ],
+                    [
+                        'image'       => public_path('product-as-denim/Katalog-Luxe-Red-Core-9.jpg'),
+                        'is_primary' => false,
+                    ],
+                ],
+            ],
+            [
+                'product_name'   => 'CORDUROY WHITE | TYPE III JACKET',
+                'category_id'    => 2,
+                'original_price' => 500000,
+                'size'           => 'S',
+                'stock'          => 1,
+                'brand'          => "Wingman",
+                'color'          => "Putih, White",
+                'weight'         => 100,
+                'description'    => 'Pada umumnya denim menggunakan benang kapas berwarna putih/natural yang di warnai ke dalam pewarna indigo. Seiring pemakaian, denim akan luntur dan permukaan luar denim akan kembali berwarna putih. Kami menambah 1 PROSES PEWARNAAN, sebelum diwarnai indigo, benang kapas terlebih dahulu diwarnai UNGU, sehingga ketika denim digunakan fading permukaan luar akan menjadi warna UNGU.',
+                'images'         => [
+                    [
+                        'image'       => public_path('product-as-denim/Corduroy-Type-III-Jacket-White-1-Cover.jpg'),
+                        'is_primary' => true,
+                    ],
+                    [
+                        'image'       => public_path('product-as-denim/Corduroy-Type-III-Jacket-White-2-510x638.jpg'),
+                        'is_primary' => false,
+                    ],
+                    [
+                        'image'       => public_path('product-as-denim/Corduroy-Type-III-Jacket-White-3-510x638.jpg'),
+                        'is_primary' => false,
+                    ],
+                    [
+                        'image'       => public_path('product-as-denim/Corduroy-Type-III-Jacket-White-6-100x100.jpg'),
+                        'is_primary' => false,
+                    ],
+                ],
+            ],
+            [
+                'product_name'   => "CTAS Classic Wedge Leather",
+                'category_id'    => 4,
+                'original_price' => 1899000,
+                'sale_price' => 1329300,
+                'size'           => 'US W8',
+                'stock'          => 1,
+                'brand'          => "Converse",
+                'color'          => "Cokelat, Brown",
+                'weight'         => 100,
+                'description'    => 'Pada umumnya denim menggunakan benang kapas berwarna putih/natural yang di warnai ke dalam pewarna indigo. Seiring pemakaian, denim akan luntur dan permukaan luar denim akan kembali berwarna putih. Kami menambah 1 PROSES PEWARNAAN, sebelum diwarnai indigo, benang kapas terlebih dahulu diwarnai UNGU, sehingga ketika denim digunakan fading permukaan luar akan menjadi warna UNGU.',
+                'images'         => [
+                    [
+                        'image'       => public_path('product-as-denim/converse-1.webp'),
+                        'is_primary' => true,
+                    ],
+                    [
+                        'image'       => public_path('product-as-denim/converse-1.webp'),
+                        'is_primary' => false,
+                    ],
+                    [
+                        'image'       => public_path('product-as-denim/converse-1.webp'),
+                        'is_primary' => false,
+                    ],
                 ],
             ],
         ];
